a9fb9ebe4df829d120943821b26e602630297ca1
Alexander Mejía <alexmejia199745@live.com>
2020-01-21 13:29:56 -0500

    Update configuration.js
    Security flaw fixed

--- Web/js/configuration.js
+++ Web/js/configuration.js
@@ -1,10 +1,3 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCVjeIUnXoM85uAjNYVbMX5qkCFsELgI5w",
-    authDomain: "tsrrcontrolmonit.firebaseapp.com",
-    databaseURL: "https://tsrrcontrolmonit.firebaseio.com",
-    projectId: "tsrrcontrolmonit",
-    storageBucket: "tsrrcontrolmonit.appspot.com",
-    messagingSenderId: "644950144407",
-    appId: "1:644950144407:web:4f74af371dc26eb0"
 };
 firebase.initializeApp(firebaseConfig);
